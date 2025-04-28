3e335f8e45dc7d6ceccca8c54d65cd22f569eeec
aliasghar1382-svg <hadi.nejad82@gmail.com>
2025-04-29 02:19:43 +0330

    Add files via upload

---
+++ index2.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(string{} args)
+    system.out.println("My first name: Aliasghar");
+    system.out.println("May last name: hadinejad");
+    system.out.println("My collage ID: 01111142302041");
+    system.out.println("My educational course: mobile programing");
+}
